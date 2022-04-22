59e0c4a9d527f9a19f307d5ad300ed53cb5d46ba
Khalil Shabana <kh.shabana1499@gamil.com>
2022-04-22 03:19:30 +0200

    adding some operations (Search/findMin/findMax/Delete) on the BST

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -24,6 +24,13 @@ public:
 	void preOrder(node* r);
 	void inOrder(node* r);
 	void postOrder(node* r);
+	node* Search(node* r, int key);
+	bool Search(int key);
+	node* findMin(node* r);
+	void findMin();
+	node* findMax(node* r);
+	void findMax();
+	node* Delete(node* r, int key);
 };
 
 
@@ -48,6 +55,33 @@ int main()
 	cout << "\n------------------\n";
 	T.postOrder(T.root);
 	cout << "\n------------------\n";
+	
+	cout << "\n#Searching...\n";
+	int key;
+	cout << "Enter item to search for :\n";
+	cin >> key;
+	(T.Search(key)) ? cout << "item found\n" << endl : cout << "sorry, item not found\n" << endl;
+
+	cout << "#Finding the min. and MAX. values in the BST\n";
+	T.findMin();
+	T.findMax();
+
+	cout << "\n\n#Deleting a node in BST\n";
+	cout << "Display the tree content before deleting \n";
+	T.preOrder(T.root);
+
+	T.root = T.Delete(T.root, 12);
+	cout << "\n\npreorder after deleting 12\n";
+	T.preOrder(T.root);
+
+	cout << "\n\npreorder after deleting 20\n";
+	T.root = T.Delete(T.root, 20);
+	T.preOrder(T.root);
+
+	cout << "\n\npreorder after deleting 12\n";
+	T.root = T.Delete(T.root, 12);
+	T.preOrder(T.root);
+
 	return 0;
 }
 
@@ -104,4 +138,101 @@ void BST::postOrder(node* r) //left -> root -> right
 		postOrder(r->right);
 		cout << r->data << " ";
 	}
+}
+
+node* BST::Search(node* r, int key)
+{
+	if (r == NULL)
+		return NULL;
+	else if (r->data == key)
+		return r;
+	else if (key < r->data)
+		return Search(r->left, key);
+	else
+		return Search(r->right, key);
+}
+
+bool BST::Search(int key)
+{
+	//node* result = Search(root, key);
+	
+	if (Search(root, key) == NULL)
+		return false;
+	else
+		return true;
+}
+
+node* BST::findMin(node* r)
+{
+	if (r == NULL)
+		return NULL;
+	else if (r->left == NULL) //no elements in the right
+		return r; //so the root is the Min
+	else
+		return findMin(r->left);
+}
+void BST::findMin()
+{
+	node* r = findMin(root);
+	if (r == NULL)
+		cout << "sorry the tree is empty!\n";
+	else
+		cout << "The min. value is = " << r->data << endl;
+}
+
+node* BST::findMax(node* r)
+{
+	if (r == NULL)
+		return NULL;
+	else if (r->right == NULL) //no elements in the right
+		return r; //so the root is the Max
+	else
+		return findMax(r->right);
+}
+void BST::findMax()
+{
+	node* r = findMax(root);
+	if (r == 0) //NULL = 0
+		cout << "sorry the tree is empty!\n";
+	else
+		cout << "The MAX. value is = " << r->data << endl;
+}
+
+node* BST::Delete(node* r, int key)
+{
+	if (Search(key)) {
+		if (r == NULL)
+			return NULL;  //Empty tree
+		else if (key < r->data)  //item exists in left sub tree
+			r->left = Delete(r->left, key);
+		else if (key > r->data)  //item exists in right sub tree
+			r->right = Delete(r->right, key);
+		else
+		{
+			if (r->left == NULL && r->right == NULL) //leaf node
+				r = NULL;
+			else if (r->left != NULL && r->right == NULL) //one child on the left 
+			{
+				r->data = r->left->data;
+				delete (r->left);
+				r->left = NULL;
+			}
+			else if (r->left == NULL && r->right != NULL)
+			{
+				r->data = r->right->data;
+				delete (r->right);
+				r->right = NULL;
+			}
+			else
+			{
+				node* MAX = findMax(r->left);
+				r->data = MAX->data;
+				r->left = Delete(r->left, MAX->data);
+			}
+		}
+		return r; //return r after deleting (new tree)
+	}
+	else
+		cout << "Element Does Not Exist !!\n";
+	return r;
 }
